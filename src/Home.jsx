1c67cfad8e5dd6020699e62153ea0eb623defeb3
IceIcons <baxtiyorovshohruz588@gmail.com>
2026-09-05 14:40:24 +0500

    bugfixes

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,12 +2,14 @@ import Listings from "./Listings";
 import Button from "@mui/material/Button";
 import { Link } from "react-router";
 import SiteHeader from "./components/SiteHeader";
+import Footer from "./components/Footer";
 
 function Home() {
   return (
     <main className="site-shell">
       <SiteHeader />
       <Listings />
+      <Footer />
       <div className="home-action">
         <Link to={`/register`}>
           <Button variant="contained">Register</Button>
---
+++ src/components/Footer.jsx
@@ -0,0 +1,59 @@
+import { Grid } from "@mui/material"
+
+const Footer = () => {
+    return (
+        <>
+            <Grid className="footerWrapper">
+                <Grid container spacing={3} className="footer">
+                    <Grid size={1} className="footerBox">
+                        <h4>Support</h4>
+                        <a href="#">Help Center</a>
+                        <a href="#">Get help with a safety issue</a>
+                        <a href="#">AirCover</a>
+                        <a href="#">Travel insurance</a>
+                        <a href="#">Anti-discrimination</a>
+                        <a href="#">Disability support</a>
+                        <a href="#">Cancellation options</a>
+                        <a href="#">Report neighborhood concern</a>
+                    </Grid>
+                    <Grid size={1} className="footerBox">
+                        <h4>Hosting</h4>
+                        <a href="#">Airbnb your home</a>
+                        <a href="#">Airbnb your experience</a>
+                        <a href="#">Airbnb your service</a>
+                        <a href="#">AirCover for Hosts</a>
+                        <a href="#">Help Center</a>
+                        <a href="#">Hosting resources</a>
+                        <a href="#">Community forum</a>
+                        <a href="#">Hosting responsibly</a>
+                        <a href="#">Airbnb-friendly apartments</a>
+                        <a href="#">Join a free hosting class</a>
+                        <a href="#">Find a co‑host</a>
+                        <a href="#">Refer a host</a>
+                    </Grid>
+                    <Grid size={1} className="footerBox">
+                        <h4>Airbnb</h4>
+                        <a href="#">2026 Summer Release</a>
+                        <a href="#">Newsroom</a>
+                        <a href="#">Careers</a>
+                        <a href="#">Investors</a>
+                        <a href="#">Gift cards</a>
+                        <a href="#">Airbnb.org emergency stays</a>
+                    </Grid>
+                </Grid><br /><br /><br />
+                <hr />
+                <Grid container spacing={2} className="footerBottom">
+                    <Grid ><span>© 2026 Airbnb, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a> · <a href="#">Your  Privacy  Choices</a></span></Grid>
+                    <Grid className="footerBottomRight"><span><span><ion-icon name="globe-outline"></ion-icon></span> English (US) $ USD </span>
+                        <span>  <ion-icon name="logo-facebook"></ion-icon></span>
+                        <span>  <ion-icon name="logo-x"></ion-icon></span>
+                        <span>  <ion-icon name="logo-instagram"></ion-icon></span>
+
+                    </Grid>
+                </Grid>
+            </Grid>
+        </>
+    )
+}
+
+export default Footer
